a51305cbde5b72e69e7521b2e660e9db34eb12b4
George Papadimitriou <georgpap@isi.edu>
2020-04-20 09:50:52 -0700

    simple working example

--- bin/wrf_wrapper.sh
+++ bin/wrf_wrapper.sh
@@ -3,6 +3,6 @@
 tar -xzf FORCING.tar.gz
 tar -xzf wrf_db.tar.gz
 
-mpirun -np $PEGASUS_CORES /example_case/NWM/wrf_hydro_NoahMP.exe
+mpirun -np 2 /example_case/NWM/wrf_hydro_NoahMP.exe
 
 tar -czf wrf_hydro_out.tar.gz diag_hydro* 2011* *\.2011*
